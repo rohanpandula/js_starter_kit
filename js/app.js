107fa9470e906d280b8ef7403c2c712dac11d404
RohanPandula <rpandula@uh.edu>
2014-02-28 14:45:22 -0500

    compelted

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-alert('Hey find me to know where to put the javascript');
+//alert('Hey find me to know where to put the javascript');
+var winning_num = Math.floor((Math.random()*100)+1)
+var user_name = prompt("What is your name?")
+var guess = prompt("Guess a random number between 1 and 100")
+
+
+
+if (guess == winning_num){
+  alert("YOU WIN! "+user_name)
+}
+else {
+  alert("You lose "+user_name)
+}
